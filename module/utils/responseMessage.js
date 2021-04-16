5f69e80a943b75b52afa29de4572bace5b4b57cf
장인규 <ig@jang-ingyuui-MacBookPro.local>
2021-04-16 15:21:51 +0900

    add findID and modify resMessage

--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -2,6 +2,20 @@ module.exports = {
   NULL_VALUE: "필요한 값이 없습니다",
   OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
 
+  DB_ERROR: "DB 오류",
+  NULL_VALUE: "필요한 값이 없습니다",
+  OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+
+  SIGNIN_SUCCESS: "로그인 성공",
+  SIGNIN_FAIL: "로그인 실패",
+  NOT_CORRECT_USERINFO: "ID 혹은 비밀번호가 일치하지 않습니다",
+  SIGNUP_SUCCESS: "회원가입 성공",
+  SIGNUP_FAIL: "회원가입 실패",
+
+  DUPLICATED_ID_FAIL: "중복된 ID가 있습니다.",
+  NOT_CORRECT_THUMB_ID: "일치하는 썸네일 ID가 없습니다.",
+
+
   MEMBERSHIP_SELECT_SUCCESS: "멤버십 조회 성공",
   MEMBERSHIP_SELECT_FAIL: "멤버십 조회 실패",
   MEMBERSHIP_INSERT_SUCCESS: "멤버십 삽입 성공",
