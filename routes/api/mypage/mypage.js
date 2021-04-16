a5637c7eb8c5fd4a965d071f505b4c5d54384e5f
장인규 <ig@jang-ingyuui-MacBookPro.local>
2021-04-16 12:12:19 +0900

    add post health

--- routes/api/mypage/mypage.js
+++ routes/api/mypage/mypage.js
@@ -11,7 +11,7 @@ const db = require('../../../module/pool');
 
 //마이페이지 상단
 router.get('/', authUtil.isLoggedin, async (req, res) => {
-    const MypageSelectQuery = 'SELECT nickname,name,stageNm,progressNM,cancerNm,disease FROM user WHERE user_id = ?'; 
+    const MypageSelectQuery = 'SELECT nickname,name,stageNm,progressNm,cancerNm,disease FROM user WHERE user_id = ?'; 
     const MypageSelectResult = await db.queryParam_Arr(MypageSelectQuery, [req.decoded.id]);
 
     console.log(req.decoded);
@@ -38,7 +38,8 @@ router.get('/profile', authUtil.isLoggedin, async (req, res) => {
 //프로필 수정
 router.put('/profile', authUtil.isLoggedin, async (req, res) => {
     const MypageUpdateProfileQuery = 'UPDATE user SET nickname =? ,name =? ,phone =? WHERE user_id = ?'; 
-    const MypageUpdateProfileResult = await db.queryParam_Arr(MypageUpdateProfileQuery , [req.decoded.id]);
+    const MypageUpdateProfileResult = await db.queryParam_Arr(MypageUpdateProfileQuery , 
+        [req.body.nickname,req.body.name,req.body.phone,req.decoded.id]);
 
     if(!MypageUpdateProfileResult){
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));     // 프로필 수정 실패
@@ -49,25 +50,30 @@ router.put('/profile', authUtil.isLoggedin, async (req, res) => {
 
 //건강데이터 목록 조회
 router.get('/health/list', authUtil.isLoggedin, async (req, res) => {
-    const MypageSelectHealthQuery = 'SELECT health_id,DATE_FORMAT(RegiDate, "%y.%m.%d") AS RegiDate,stageNm,progressNM,cancerNm,disease,weight,height FROM user WHERE user_id = ?'; 
+    const MypageSelectHealthQuery = 'SELECT health_id,DATE_FORMAT(RegiDate, "%y.%m.%d") AS RegiDate,stageNm,progressNm,cancerNm,disease,weight,height FROM user WHERE user_id = ?'; 
     const MypageSelectHealthResult = await db.queryParam_Arr(MypageSelectHealthQuery, [req.decoded.id]);
 
     if(!MypageSelectHealthResult ){
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));     // 건강데이터 목록 조회 실패
     }else{
-        res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.SUCCESS_USER_LIST, MypageSelectHealthResult ));      // 건강데이터 목록  조회 성공
+        res.status(200).send(defaultRes.successTrue(statusCode.OK, "건강데이터 목록 조회 성공", MypageSelectHealthResult));      // 건강데이터 목록  조회 성공
     }
 });
 
 //건강데이터 입력
 router.post('/health', authUtil.isLoggedin, async (req, res) => {
-    const MypageSelectQuery = 'SELECT nickname,name,stageNm,progressNM,cancerNm,disease FROM user WHERE user_id = ?'; 
-    const MypageSelectResult = await db.queryParam_Arr(MypageSelectQuery, [req.decoded.id]);
+    const MypageInsertHealthQuery = "INSERT INTO user_health " 
+    +"(user_id,relationNm,gender,age,height,weight,stageNm,progressNm,cancerNm,disease,disable_food,memo,RegiDate) "
+    +"INTO "
+    +"(?,?,?,?,?,?,?,?,?,?,?,?,NOW()) "
+    const MypageInsertHealthResult = await db.queryParam_Arr(MypageInsertHealthQuery, 
+        [req.decoded.id,req.body.relationNm,req.body.gender,req.body.age,req.body.height,req.body.weight,req.body.stageNm
+        ,req.body.progressNm,req.body.cancerNm,req.body.disease,req.body.disable_food,req.body.memo]);
 
-    if(!MypageSelectQuery){
-        res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));     // 회원정보 조회 실패
+    if(!MypageInsertHealthResult){
+        res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));     // 건강데이터 입력 실패
     }else{
-        res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.SUCCESS_USER_LIST, modifySelectResult));      // 회원정보 조회 성공
+        res.status(200).send(defaultRes.successTrue(statusCode.OK, "건강데이터 입력 성공"));      // 건강데이터 입력 성공
     }
 });
 //건강데이터 상세
