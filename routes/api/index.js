2bfdaf3ae44c1a719f9bad621362b59201783a13
장인규 <ig@jang-ingyuui-MacBookPro.local>
2021-05-08 15:47:36 +0900

    add main#2

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ var router = express.Router();
 router.use('/mypage', require('./mypage/index'));
 router.use('/auth', require('./auth/index'));
 router.use('/search', require('./search/index'));
-
+router.use('/main', require('./main/index'));
 
 module.exports = router;
