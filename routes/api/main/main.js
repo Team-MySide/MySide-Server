e3d24c1c4b104dcca11675474b8ff96e9cf1efd3
장인규 <ig@jang-ingyuui-MacBookPro.local>
2021-05-14 21:05:44 +0900

    add main#2

--- routes/api/main/main.js
+++ routes/api/main/main.js
@@ -8,70 +8,66 @@ const statusCode = require('../../../module/utils/statusCode');
 const resMessage = require('../../../module/utils/responseMessage');
 const db = require('../../../module/pool');
 
-router.get('/recommendation', authUtil.isLoggedin,async (req, res) => {
-
-    const SelectCancerQuery = 'SELECT cancerNm FROM user WHERE user_id = ?'; 
-    const SelectCancerResult = await db.queryParam_Arr(SelectCancerQuery, [req.decoded.id]);
-
-    if(!SelectCancerResult){
-        res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));  
-    }else{
-        let cancerNm = SelectCancerResult[0].cancerNm;
-        const SelectQuery = 
+router.get('/cancer/recommendation',async (req, res) => {
+    
+    let SelectCancerResult = [];
+    const SelectQuery1 = 
         'SELECT food_id, name,img,category,cancerNm,background_color,wishes,views,likes,nutrition1 '
         + 'FROM food_thumbnail A, cancer_food B '
         + 'WHERE A.name = B.food '
-        + 'AND B.cancerNm = ? '
-        + 'LIMIT 5 '; 
-        const SelectResult = await db.queryParam_Arr(SelectQuery, [cancerNm]);
+        + "AND B.cancerNm = '위암' "
+        + 'LIMIT 2 '; 
+    const SelectResult1 = await db.queryParam_Arr(SelectQuery1);
+    const SelectQuery2 = 
+        'SELECT food_id, name,img,category,cancerNm,background_color,wishes,views,likes,nutrition1 '
+        + 'FROM food_thumbnail A, cancer_food B '
+        + 'WHERE A.name = B.food '
+        + "AND B.cancerNm = '간암' "
+        + 'LIMIT 2 '; 
+    const SelectResult2 = await db.queryParam_Arr(SelectQuery2);
+    const SelectQuery3 = 
+        'SELECT food_id, name,img,category,cancerNm,background_color,wishes,views,likes,nutrition1 '
+        + 'FROM food_thumbnail A, cancer_food B '
+        + 'WHERE A.name = B.food '
+        + "AND B.cancerNm = '대장암' "
+        + 'LIMIT 2 '; 
+    const SelectResult3 = await db.queryParam_Arr(SelectQuery3);
+    const SelectQuery4 = 
+        'SELECT food_id, name,img,category,cancerNm,background_color,wishes,views,likes,nutrition1 '
+        + 'FROM food_thumbnail A, cancer_food B '
+        + 'WHERE A.name = B.food '
+        + "AND B.cancerNm = '폐암' "
+        + 'LIMIT 2 '; 
+    const SelectResult4 = await db.queryParam_Arr(SelectQuery4);
+    const SelectQuery5 = 
+        'SELECT food_id, name,img,category,cancerNm,background_color,wishes,views,likes,nutrition1 '
+        + 'FROM food_thumbnail A, cancer_food B '
+        + 'WHERE A.name = B.food '
+        + "AND B.cancerNm = '유방암' "
+        + 'LIMIT 2 '; 
+    const SelectResult5 = await db.queryParam_Arr(SelectQuery5);
+    const SelectQuery6 = 
+        'SELECT food_id, name,img,category,cancerNm,background_color,wishes,views,likes,nutrition1 '
+        + 'FROM food_thumbnail A, cancer_food B '
+        + 'WHERE A.name = B.food '
+        + "AND B.cancerNm = '갑상선암' "
+        + 'LIMIT 2 '; 
+    const SelectResult6 = await db.queryParam_Arr(SelectQuery6);
 
-        res.status(200).send(defaultRes.successTrue(statusCode.OK, "추천 음식 조회 성공", SelectResult));     
-    }
-});
-
-
-router.get('/rank/', async (req, res) => {
-
-    let SelectQuery = 
-    'SELECT food_id, name,img,category,cancerNm,background_color,wishes,views,likes,nutrition1 '
-    + 'FROM food_thumbnail A, cancer_food B '
-    + 'WHERE A.name = B.food '
-    + 'ORDER BY likes '
-
-
-    let SelectResult = await db.queryParam_None(SelectQuery);
-
-    if(!SelectRankResult){
+    SelectCancerResult.push(SelectResult1);
+    SelectCancerResult.push(SelectResult2);
+    SelectCancerResult.push(SelectResult3);
+    SelectCancerResult.push(SelectResult4);
+    SelectCancerResult.push(SelectResult5);
+    SelectCancerResult.push(SelectResult6);
+    if(!SelectCancerResult){
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));  
     }else{
-        res.status(200).send(defaultRes.successTrue(statusCode.OK, "랭킹 조회 성공", SelectResult));    
+        res.status(200).send(defaultRes.successTrue(statusCode.OK, "질병별 추천 음식 성공", SelectCancerResult));     
     }
 });
 
 
-
-router.get('/rank/:tabIdx', async (req, res) => {
-
-    let SelectRankQuery;
-    let SelectQuery = 
-    'SELECT food_id, name,img,category,cancerNm,background_color,wishes,views,likes,nutrition1 '
-    + 'FROM food_thumbnail A, cancer_food B '
-    + 'WHERE A.name = B.food '
-    + 'ORDER BY likes '
-    if(req.params.tabIdx == 0){//좋아요
-       SelectRankQuery = SelectQuery + 'ORDER BY likes '
-    }else{ // 추천순 
-        SelectRankQuery = SelectQuery + 'ORDER BY views '
-    }
-
-    let SelectRankResult = await db.queryParam_None(SelectRankQuery);
-
-    if(!SelectRankResult){
-        res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));  
-    }else{
-        res.status(200).send(defaultRes.successTrue(statusCode.OK, "랭킹 조회 성공", SelectRankResult));    
-    }
-});
 
 
 
