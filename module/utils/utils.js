e24527a29258f5560fc6f0a9e0dc5f1de98c3738
장인규 <ig@jang-ingyuui-MacBookPro.local>
2021-05-30 14:26:41 +0900

    rollback merge#5

--- module/utils/utils.js
+++ module/utils/utils.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const authUtil = {
     successTrue: (status, message, data) => {
         return {
@@ -18,25 +17,4 @@ const authUtil = {
     }
 };
 
-=======
-const authUtil = {
-    successTrue: (status, message, data) => {
-        return {
-            status: status,
-            success: true,
-            message: message,
-            data: data
-        }
-    },
-    successFalse: (status, message) => {
-
-        return {
-            status: status,
-            success: false,
-            message: message
-        }
-    }
-};
-
->>>>>>> 5fe080a8889eb343eca8528bd7e1b1cd7ec9e8d8
 module.exports = authUtil;
