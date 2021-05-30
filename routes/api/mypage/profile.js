5fe080a8889eb343eca8528bd7e1b1cd7ec9e8d8
bohyeon <980926a@gmail.com>
2021-05-30 11:14:29 +0900

    bohyeon0530_mypageprofile

--- routes/api/mypage/profile.js
+++ routes/api/mypage/profile.js
@@ -30,7 +30,7 @@ const { Health } = require('aws-sdk');
 // });
 /* (1) 프로필 조회 */
 //세가지데이터 가져온다 : nickname, email, phone
-router.get('/profile',authUtil.isLoggedin,async(req,res)=>{//비동기처리로 /profile 링크에서 로그인된 상태로 들어왔을 때 처리하는 api
+router.get('/',authUtil.isLoggedin,async(req,res)=>{//비동기처리로 /profile 링크에서 로그인된 상태로 들어왔을 때 처리하는 api
     const MyPageSelectProfileQuery = 'SELECT nickname,email,CONCAT( left(phone,3) , "-" , mid(phone,4,4) , "-", right(phone,4)) AS phone FROM user WHERE user_id = ?';//user_id의 값으로 필요한 데이터를 가져온다
     //gui에서 보면, 세가지 데이터 필요 : nickname, email, phone
     //01012345678을 010-1234-5678출력위해, CONCAT SUBSTRING (컬럼명 from i to j),'-' 사용
@@ -56,7 +56,7 @@ router.get('/profile',authUtil.isLoggedin,async(req,res)=>{//비동기처리로 
 // });
 /* (2) 닉네임 수정 */
 //nickname UPDATE
-router.put('/profile', authUtil.isLoggedin, async (req, res) => {
+router.put('/nickname', authUtil.isLoggedin, async (req, res) => {
     const MypageUpdateProfileQuery = 'UPDATE user SET nickname =? WHERE user_id = ?'; 
     const MypageUpdateProfileResult = await db.queryParam_Arr(MypageUpdateProfileQuery , 
         [req.body.nickname,req.decoded.id]);
@@ -69,16 +69,16 @@ router.put('/profile', authUtil.isLoggedin, async (req, res) => {
 });
 
 /* (3-1) 비밀번호 확인 */
-router.post('/profile/checkpw',authUtil.isLoggedin, async (req, res) => {
+router.post('/checkpw',authUtil.isLoggedin, async (req, res) => {
     password = req.body.password;
     const MyPageCheckPWQuery = 'SELECT * FROM user WHERE user_id = ?';//user_id의 값으로 password값 가져온다
     const MyPageCheckPWResult = await db.queryParam_Arr(MyPageCheckPWQuery, user_id);//req에서 토큰으로 decoded된 id과 쿼리문 await으로 비동기 기다리기
     if(!MyPageCheckPWResult){
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));     //존재하는 result 형식이용해서 실패값 출력
     }else{
-        const salt = selectUserResult[0].salt;
+        const salt = MyPageCheckPWQuery[0].salt;
         const hashedEnterPw = await crypto.pbkdf2(password, salt, 1000, 32, 'SHA512')
-        const dbPw = selectUserResult[0].password
+        const dbPw = MyPageCheckPWQuery[0].password
         if (hashedEnterPw.toString('base64') == dbPw) {
             res.status(200).send(defaultRes.successTrue(statusCode.OK, "비밀번호 조회 성공", MypageSelecProfiletResult[0]));
         }
@@ -89,7 +89,7 @@ router.post('/profile/checkpw',authUtil.isLoggedin, async (req, res) => {
     }
 });
 /* (3-2) 휴대폰번호 변경 */
-router.put('/profile/checkpw/changepb', authUtil.isLoggedin, async (req, res) => {
+router.put('/changepb', authUtil.isLoggedin, async (req, res) => {
     const MypageUpdateProfileQuery = 'UPDATE user SET phone =? WHERE user_id = ?'; 
     const MypageUpdateProfileResult = await db.queryParam_Arr(MypageUpdateProfileQuery , 
         [req.body.phone,req.decoded.id]);
