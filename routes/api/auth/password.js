375b476c06760c5003877a70577b6229c2954b3a
developer-Wonjun <iwonjun97@naver.com>
2021-06-06 19:40:29 +0900

    wonjun

--- routes/api/auth/password.js
+++ routes/api/auth/password.js
@@ -4,38 +4,32 @@ const defaultRes = require('../../../module/utils/utils');
 const statusCode = require('../../../module/utils/statusCode');
 const resMessage = require('../../../module/utils/responseMessage')
 const db = require('../../../module/pool');
-const bcrypt = require('bcrypt');
+const crypto = require('crypto-promise');
 const jwtUtils = require('../../../module/jwt');
 const nodemailer = require('nodemailer');        // e-mail 보낼 때 사용
 const senderInfo = require('../../../config/senderInfo.json');
-
+const { hash } = require('crypto-promise');
+const saltRounds = 10;
 
 
 router.get('/:email/:name', async (req, res) => {
 
-    console.log(req.params);
-    const getUser = "SELECT * FROM user WHERE email=? AND name =? ";
-    const getUserinfo = await db.queryParam_Parse(getUser, [req.params.email, req.params.name]);
-  
-  
-  
-    if (!getUserinfo[0]) {
-        res.status(200).send(defaultRes.successFalse(statusCode.OK, resMessage.NOT_CORRECT_INFO));      // 올바르지 않은 정보 입니다
-    } else { //쿼리문이 성공했을 때
-  
-      //f 추출
-      var random = Math.random().toString(36).substr(2,11); //비밀번호 암호화
-      const encryptedPassword = bcrypt.hashSync(random, 10);
-  
-      const getUpdate = "UPDATE myside.user SET password =? WHERE email =? AND name = ?";
-      const getResult = await db.queryParam_Parse(getUpdate, [encryptedPassword, req.params.email, req.params.name]);
-  
-      let email = req.params.email;
-      var password = random;
-      console.log(email);
-      console.log(password);
-  
-  
+  const userInfoQurey = "SELECT * from user WHERE email = ? AND name = ?";
+  const userInfoResult = await db.queryParam_Parse(userInfoQurey, [req.params.email, req.params.name]); //유저 입력 정보
+  if (!userInfoResult) {  //False
+    res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR)); 
+  }
+  else {//True --> 임시비밀번호 랜덤으로 생성하여 발송 && 암호화하여 디비 저장
+      const password = Math.random().toString(36).slice(2); // 랜덤 임시비밀번호 난수 생성
+      const salt = userInfoResult[0].salt; // salt 업데이트
+
+      const hashPassword = await crypto.pbkdf2(password, salt, 1000, 32, 'SHA512')
+      const tempPWD = hashPassword.toString('base64') //임시비밀번호 암호화
+
+      const changePasswordQuery = "UPDATE user SET password = ? WHERE name = ? AND email = ?";
+      const changePasswordResult = await db.queryParam_Parse(changePasswordQuery, [tempPWD, req.params.name, req.params.email]);
+
+
       const smtpTransport = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
@@ -43,26 +37,25 @@ router.get('/:email/:name', async (req, res) => {
           pass: senderInfo.pass  // 보내는 메일의 비밀번호
         }
       })
-      
+
       const mailOptions = {
         from: senderInfo.user,
-        to: email,
+        to: req.params.email,
         subject: '안녕하세요. 이웃집닥터입니다.',
         text: "임시 비밀번호 : " + password
       };
-  
+
       await smtpTransport.sendMail(mailOptions, (error, responses) => {
         if (error) {
           console.log(error);
           res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.FIND_PASSWORD_FAIL));      // 올바르지 않은 정보 입니다
         } else {
-            res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.FIND_PASSWORD_SUCCESS, getResult[0]));      // 아이디 찾기 성공 
+          res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.FIND_PASSWORD_SUCCESS, changePasswordResult));      // 아이디 찾기 성공 
         }
         smtpTransport.close();
       });
     }
   });
-  
-  
-  module.exports = router;
-  
+
+
+module.exports = router;
