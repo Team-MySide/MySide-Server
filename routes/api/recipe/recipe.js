3e5c58b8203c8b73601c1e86d9194c3d78d564a8
bohyeon <980926a@gmail.com>
2022-04-26 15:37:12 +0900

    수정 : 레시피 입력 API 주석처리 해제

--- routes/api/recipe/recipe.js
+++ routes/api/recipe/recipe.js
@@ -141,125 +141,125 @@ router.post('/insert_type', async (req, res) => {
 
 
 // 레시피 입력하기
-// router.post('/insert/:recipe_id', upload.array('img',31),async (req, res) => {
+router.post('/insert/:recipe_id', upload.array('img',31),async (req, res) => {
 
-//     // 2. 레시피 요리순서(글 이미지)
-//     receipe_list = req.body.receipe_list;
-//     const InsertCookStepQuery = 
-//     'INSERT INTO receipe_step (receipe_id, step_num, step_content, step_img, regiDate) VALUES (?,?,?,?,?)'
+    // 2. 레시피 요리순서(글 이미지)
+    receipe_list = req.body.receipe_list;
+    const InsertCookStepQuery = 
+    'INSERT INTO receipe_step (receipe_id, step_num, step_content, step_img, regiDate) VALUES (?,?,?,?,?)'
 
-//     for(var key in receipe_list){
-//         console.log(key, typeof receipe_list[key])
-//         data = JSON.parse(receipe_list[key])
+    for(var key in receipe_list){
+        console.log(key, typeof receipe_list[key])
+        data = JSON.parse(receipe_list[key])
         
-//         // console.log(typeof data)
-//         // console.log("receipe_list[key].step_num", data.step_num)
+        // console.log(typeof data)
+        // console.log("receipe_list[key].step_num", data.step_num)
 
-//         if(req.files[key]){
-//             InsertReceipeResult2 = await db.queryParam_Arr(InsertCookStepQuery, [
-//                 req.params.recipe_id,
-//                 data.step_num,
-//                 data.step_content,
-//                 req.files[key].location,
-//                 moment().format('YYYY-MM-DD HH:mm:ss'),
-//             ]);
+        if(req.files[key]){
+            InsertReceipeResult2 = await db.queryParam_Arr(InsertCookStepQuery, [
+                req.params.recipe_id,
+                data.step_num,
+                data.step_content,
+                req.files[key].location,
+                moment().format('YYYY-MM-DD HH:mm:ss'),
+            ]);
 
-//         }else{
-//             InsertReceipeResult2 = await db.queryParam_Arr(InsertCookStepQuery, [
-//                 req.params.recipe_id,
-//                 data.step_num,
-//                 data.step_content,
-//                 '',
-//                 moment().format('YYYY-MM-DD HH:mm:ss'),
-//             ]);
-//         }
-//     }
+        }else{
+            InsertReceipeResult2 = await db.queryParam_Arr(InsertCookStepQuery, [
+                req.params.recipe_id,
+                data.step_num,
+                data.step_content,
+                '',
+                moment().format('YYYY-MM-DD HH:mm:ss'),
+            ]);
+        }
+    }
 
-//       console.log('InsertReceipeResult2', InsertReceipeResult2);
-//     // 2-2 요리 순서 (영상)
-//     link = req.body.link
-//     link_content = req.body.link_content
+      console.log('InsertReceipeResult2', InsertReceipeResult2);
+    // 2-2 요리 순서 (영상)
+    link = req.body.link
+    link_content = req.body.link_content
 
-//     const InsertReceipeStepQuery = 
-//     'UPDATE receipe SET link= ?, link_comment = ? WHERE receipe_id = ?'
-//     const InsertReceipeStepResult = await db.queryParam_Arr(InsertReceipeStepQuery, [link, link_content, req.params.recipe_id]);
-//     console.log(InsertReceipeStepResult);
+    const InsertReceipeStepQuery = 
+    'UPDATE receipe SET link= ?, link_comment = ? WHERE receipe_id = ?'
+    const InsertReceipeStepResult = await db.queryParam_Arr(InsertReceipeStepQuery, [link, link_content, req.params.recipe_id]);
+    console.log(InsertReceipeStepResult);
 
-//     // 3. 재료입력
-//     ingredient_list = req.body.ingredient_list;
+    // 3. 재료입력
+    ingredient_list = req.body.ingredient_list;
 
-//     insertIngredQuery = 'INSERT INTO receipe_ingredient' + '(receipe_id,ingredient_content, ingredient_type,  regidate)' + 'VALUES(?,?,?,?)' 
+    insertIngredQuery = 'INSERT INTO receipe_ingredient' + '(receipe_id,ingredient_content, ingredient_type,  regidate)' + 'VALUES(?,?,?,?)' 
 
-//     for(var key in ingredient_list){
-//         console.log(key, typeof ingredient_list[key])
-//         data = JSON.parse(ingredient_list[key])
+    for(var key in ingredient_list){
+        console.log(key, typeof ingredient_list[key])
+        data = JSON.parse(ingredient_list[key])
 
-//         console.log(typeof data)
-//         console.log("ingredient_list[key].ingredient_content", data.ingredient_content)
+        console.log(typeof data)
+        console.log("ingredient_list[key].ingredient_content", data.ingredient_content)
 
-//         InsertReceipeResult3 = await db.queryParam_Arr(insertIngredQuery, [
-//             req.params.recipe_id,
-//             data.ingredient_content,
-//             data.ingredient_type,
-//             moment().format('YYYY-MM-DD HH:mm:ss'),
-//         ]);
-//     }
+        InsertReceipeResult3 = await db.queryParam_Arr(insertIngredQuery, [
+            req.params.recipe_id,
+            data.ingredient_content,
+            data.ingredient_type,
+            moment().format('YYYY-MM-DD HH:mm:ss'),
+        ]);
+    }
 
-//     console.log('InsertReceipeResult3', InsertReceipeResult3);
-//     console.log('req.files.length ; ', req.files.length);
-//     lastnum = req.files.length-1
-//     // 4. 레시피 입력
-//     receipe_name = req.body.receipe_name
-//     receipe_content = req.body.receipe_content
+    console.log('InsertReceipeResult3', InsertReceipeResult3);
+    console.log('req.files.length ; ', req.files.length);
+    lastnum = req.files.length-1
+    // 4. 레시피 입력
+    receipe_name = req.body.receipe_name
+    receipe_content = req.body.receipe_content
 
-//     receipe_foodtype = req.body.receipe_foodtype
-//     receipe_difficulty =req.body.receipe_difficulty
-//     receipe_time = req.body.receipe_time
-//     receipe_volume = req.body.receipe_volume
-//     receipe_caution = req.body.receipe_caution
-//     receipe_fakesave = req.body.receipe_fakesave
+    receipe_foodtype = req.body.receipe_foodtype
+    receipe_difficulty =req.body.receipe_difficulty
+    receipe_time = req.body.receipe_time
+    receipe_volume = req.body.receipe_volume
+    receipe_caution = req.body.receipe_caution
+    receipe_fakesave = req.body.receipe_fakesave
 
-//     const InsertReceipeQuery = 
-//     'UPDATE receipe SET receipe_name=?, receipe_content=?, receipe_img=?,' + 
-//     ' receipe_foodtype=?, receipe_difficulty=?,receipe_time=?, receipe_volume=?, '+
-//     ' receipe_caution=?, receipe_mainfood=?,'+
-//     'receipe_fakesave=?, regiDate=? WHERE receipe_id = ?;'
+    const InsertReceipeQuery = 
+    'UPDATE receipe SET receipe_name=?, receipe_content=?, receipe_img=?,' + 
+    ' receipe_foodtype=?, receipe_difficulty=?,receipe_time=?, receipe_volume=?, '+
+    ' receipe_caution=?, receipe_mainfood=?,'+
+    'receipe_fakesave=?, regiDate=? WHERE receipe_id = ?;'
    
-//     const InsertReceipeResult4 = await db.queryParam_Arr(InsertReceipeQuery, [receipe_name, receipe_content, req.files[lastnum].location, 
-//         receipe_foodtype, receipe_difficulty,receipe_time, receipe_volume, 
-//         receipe_caution,req.body.receipe_mainfood,
-//         receipe_fakesave, moment().format('YYYY-MM-DD HH:mm:ss'), req.params.recipe_id]);
-//     console.log(InsertReceipeResult4);
+    const InsertReceipeResult4 = await db.queryParam_Arr(InsertReceipeQuery, [receipe_name, receipe_content, req.files[lastnum].location, 
+        receipe_foodtype, receipe_difficulty,receipe_time, receipe_volume, 
+        receipe_caution,req.body.receipe_mainfood,
+        receipe_fakesave, moment().format('YYYY-MM-DD HH:mm:ss'), req.params.recipe_id]);
+    console.log(InsertReceipeResult4);
     
-//     //  5. 부재료 입력하기
+    //  5. 부재료 입력하기
   
-//     ingredient_content = req.body.ingredient_content;
+    ingredient_content = req.body.ingredient_content;
  
-//     insertIngredQuery = 'INSERT INTO receipe_subingredient' + '(receipe_id, content,  regidate)' + 'VALUES(?,?,?)' 
+    insertIngredQuery = 'INSERT INTO receipe_subingredient' + '(receipe_id, content,  regidate)' + 'VALUES(?,?,?)' 
 
 
-//     for(var key in ingredient_content){
+    for(var key in ingredient_content){
 
-//         console.log("ingredient_content", ingredient_content[key])
+        console.log("ingredient_content", ingredient_content[key])
 
-//         InsertReceipeResult5 = await db.queryParam_Arr(insertIngredQuery, [
-//             req.params.recipe_id,
-//             ingredient_content[key],
-//             moment().format('YYYY-MM-DD HH:mm:ss'),
-//         ]);    
-//     }
+        InsertReceipeResult5 = await db.queryParam_Arr(insertIngredQuery, [
+            req.params.recipe_id,
+            ingredient_content[key],
+            moment().format('YYYY-MM-DD HH:mm:ss'),
+        ]);    
+    }
 
-//     console.log('InsertReceipeResult', InsertReceipeResult5);
+    console.log('InsertReceipeResult', InsertReceipeResult5);
 
 
-//      if (!(InsertReceipeResult2 && InsertReceipeResult3 && InsertReceipeResult4 && InsertReceipeResult5)) { //DB에러
-//          res.status(200).send(defaultRes.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.DB_ERROR));        
-//      }
-//      else {//데이터베이스 연결 성공
-//              res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.RECIPE_INSERT ));
-//      }
+     if (!(InsertReceipeResult2 && InsertReceipeResult3 && InsertReceipeResult4 && InsertReceipeResult5)) { //DB에러
+         res.status(200).send(defaultRes.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.DB_ERROR));        
+     }
+     else {//데이터베이스 연결 성공
+             res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.RECIPE_INSERT ));
+     }
      
-//  });
+ });
 
 
 module.exports = router;
